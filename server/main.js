8e15b1be4e9391d0f7a4638733346b7ac262213f
DESKTOP-NN1THI7\choi <chj4535@ajou.ac.kr>
2018-11-24 00:59:44 +0900

    소켓 연결 추가

--- server/main.js
+++ server/main.js
@@ -23,6 +23,74 @@ server.listen(port);
 server.on('error', onError);
 server.on('listening', onListening);
 
+/*소켓용 테스트*/
+
+const mongodb =require('mongodb');
+const MongoClient = mongodb.MongoClient;
+var io = require('socket.io').listen(3100);
+var channel;
+const dbName = 'db';
+
+io.on('connection', function (socket) {
+    console.log('connect');
+    var instanceId = socket.id;
+
+    socket.on('joinRoom',function (data) {
+        console.log(data);
+        socket.join(data.channel);
+        channel = data.channel;
+        console.log(channel);
+        MongoClient.connect('mongodb://localhost:27017/', function (error, client) {
+            console.log(channel);
+            if (error) console.log(error);
+            else {
+                const db = client.db(dbName);
+                db.collection('log').find({channel:channel}).sort({data:1}).toArray(function(err,doc){
+                    if (err) console.log(err);
+                    doc.forEach(function(item){
+                        console.log(item);
+                        io.sockets.in(channel).emit('recMsg', {comment:item});
+                    });
+                    console.log("소켓입장확인");
+                    let msg={msg:socket.handshake.address+"님이 "+channel+" 채널에 입장하셨습니다."};
+                    console.log(msg);
+                    io.sockets.in(channel).emit('recMsg', {comment:msg});
+                    client.close();
+                });
+            }
+        });
+    });
+
+    socket.on('send', function (data) {
+        console.log('data :', data)
+        let dataAddinfo = {ip: socket.handshake.address, msg: data.comment, date: Date.now()};
+        console.log(dataAddinfo)
+        MongoClient.connect('mongodb://localhost:27017/', function (error, client) {
+            if (error) console.log(error);
+            else {
+                const db = client.db(dbName);
+                db.collection('log').insert({
+                    ip: dataAddinfo.ip,
+                    msg: dataAddinfo.msg,
+                    date: dataAddinfo.date,
+                    channel: data.channel
+                }, function (err, doc) {
+                    if (err) console.log(err);
+                    client.close();
+                });
+            }
+        });
+        io.sockets.in(data.channel).emit('recMsg', {comment: dataAddinfo});
+    });
+
+    socket.on('reqMsg', function (data) {
+        console.log(data);
+        io.sockets.in(channel).emit('recMsg', {comment : data.comment+'\n'});
+    })
+});
+
+/*소켓용 테스트*/
+
 /**
  * Normalize a port into a number, string, or false.
  */
