63077fa0b3f74e46bde6c56b6590dee261a2759e
choi <chj4535@ajou.ac.kr>
2018-12-07 17:32:53 +0900

    채팅 다시 통합

--- src/components/GroupProject/Chat/Chat.js
+++ src/components/GroupProject/Chat/Chat.js
@@ -1,5 +1,4 @@
 import React, {Component} from 'react';
-import ChatList from './ChatList';
 import './Chat.css'
 
 ////스타일 CSS로 이동
@@ -46,9 +45,64 @@ class Chat extends Component {
         this.setState({ msg: event.target.value });
     }
     render() {
-        let list = this.state.chatList.map((item, index) => 
-            ChatList(this.state.email, this.state.email, item, index, this.state.email)
-        );
+        var my_email=this.state.email;
+        var pre_email=this.state.email;
+        let list = this.state.chatList.map((item, index) =>{
+            let date= new Date(item.comment.date);
+            let now_email = item.comment.email; // 현재 이메일
+            function addZero(i) { // 시간이 한자리 일때 앞에 0추가해줌!
+                if (i < 10) {
+                    i = "0" + i;
+                }
+                return i;
+            }
+            function emailcheck() {
+                if(pre_email!=now_email){
+                    pre_email=now_email
+                    return(
+                        <p style={{margin:'5px'}}>{item.comment.username}</p>
+                    )
+                }
+            }
+            function mycheck() {
+                if(item.comment.email==my_email){
+                    return(
+                        <div>
+                            {addZero(date.getHours())}:{addZero(date.getMinutes())}
+                            <div className={item.comment.email==my_email?"balloonY":"balloonN"}>
+                                <div className="msg_text">
+                                {item.comment.msg}
+                                </div>
+                            </div>
+                        </div>
+                    )
+                }
+                else{
+                    return(
+                        <div>
+                            <div className={item.comment.email==my_email?"balloonY":"balloonN"}>
+                                <div className="msg_text">
+                                    {item.comment.msg}
+                                </div>
+                            </div>
+                            {addZero(date.getHours())}:{addZero(date.getMinutes())}
+                        </div>
+                    )
+                }
+            }
+            return(
+                <div key={index}>
+                    <div className="chattingView-msgline">
+                        <div style={item.comment.email==this.state.email?{textAlign:"right"}:{textAlign:"left"}}>
+                            <div className="chattingView-msgbox">
+                                {emailcheck()}
+                                {mycheck()}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            )
+        });
 
         //렌더 부분
         return (
